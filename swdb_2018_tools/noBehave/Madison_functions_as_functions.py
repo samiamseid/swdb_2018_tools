9a2787c7e41f824841dee8be11ccb9848f3bf3f3
Sam Seid <samiamseid@gmail.com>
2018-08-30 10:31:45 -0700

    I swear i know what im doing

--- swdb_2018_tools/noBehave/Madison_functions_as_functions.py
+++ swdb_2018_tools/noBehave/Madison_functions_as_functions.py
@@ -1,71 +1,12 @@
 from __future__ import print_function
 def eng_window(trials,
-               catch=False,
-               preview = False,
-               change_1 = 0.75,
-               change_2 = 0.15,
-               pre_change = False,
-               all_pre_change = False, 
-               meta = False):
-eng_st = []
-eng_end = []
-change_image_list = []
-if pre_change == True:
-    pre_eng_st = []
-    pre_eng_end = []
-    
-for i in range(len(trials.trial)):
-    if catch == False:
-        if trials.trial_type[i] == 'go':
-            last_initial_image =(trials.change_time[i] - change_1)
-            eng_st.append(last_initial_image)
-            resp_window_start = (trials.change_time[i] + change_2)
-            eng_end.append(resp_window_start) 
-            
-            if pre_change ==True:
-                pre_last_initial_image =(last_initial_image - 0.75)
-                pre_eng_st.append(pre_last_initial_image)
-                pre_resp_window_start = (resp_window_start - 0.75)
-                pre_eng_end.append(pre_resp_window_start)
-                
-                if all_pre_change == True:
-                    trial_length =  trials.trial_length[i]
-                    repeats = int(trial_length / 0.75)
-                    way_back = 1
-                    while repeats > 3:
-                        pre_last_initial_image =(last_initial_image - (0.75+way_back))
-                        pre_eng_st.append(pre_last_initial_image)
-                        pre_resp_window_start = (resp_window_start - (0.75+way_back))
-                        pre_eng_end.append(pre_resp_window_start)
-                        repeats -=1
-                        way_back += 1
-            if meta == True:
-                change_image = trials.change_image_name[i]
-                change_image_list.append(change_image)
-    if catch== True:
-        last_initial_image =(trials.change_time[i] - change_1)
-        eng_st.append(last_initial_image)
-        resp_window_start = (trials.change_time[i] + change_2)
-        eng_end.append(resp_window_start) 
-
-if preview == True:
-    
-    print("preview: ", eng_st[:3], eng_end[:3], "length is: ", len(eng_st))
-if pre_change == True:
-    return(eng_st, eng_end, pre_eng_st, pre_eng_end)
-if image == True:
-    return(eng_st, eng_end, change_image_list)
-else:
-    return(eng_st, eng_end)
-
-    def eng_window(trials,
-               catch=False,
-               preview = False,
-               change_1 = 0.75,
-               change_2 = 0.15,
-               pre_change = False,
-               all_pre_change = False, 
-               meta = False):
+            catch=False,
+            preview = False,
+            change_1 = 0.75,
+            change_2 = 0.15,
+            pre_change = False,
+            all_pre_change = False, 
+            meta = False):
     eng_st = []
     eng_end = []
     change_image_list = []
@@ -106,7 +47,7 @@ else:
             eng_st.append(last_initial_image)
             resp_window_start = (trials.change_time[i] + change_2)
             eng_end.append(resp_window_start) 
-    
+
     if preview == True:
         
         print("preview: ", eng_st[:3], eng_end[:3], "length is: ", len(eng_st))
@@ -115,7 +56,7 @@ else:
     if image == True:
         return(eng_st, eng_end, change_image_list)
     else:
-        return(eng_st, eng_end)
+        return(eng_st, eng_end) 
 
  #wide engagement binary for 2 successes in 3 consecutive trials 
 #1 out in each direction
